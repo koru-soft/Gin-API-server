4d5f93d47577a0bb2c398c23aa0341b284cfcc3d
asai <bigrj7ao9kl@gmail.com>
2022-12-28 23:51:13 +0900

    update: ユーザーエージェントを取得できるよう修正

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
+	userAgent := ""
+
+	engine.Use(func(c * gin.Context) {
+		userAgent = c.GetHeader("User-Agent")
+		c.Next()
+	})
+
 	engine.GET("/", func(c * gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello world",
+			"User-Agent": userAgent,
 		})
 	})
 	engine.Run(":3000")
