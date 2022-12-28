543acafdf01c542d2bdf937e536712c723ff322e
asai <bigrj7ao9kl@gmail.com>
2022-12-29 02:41:36 +0900

    main.goを編集し、REST-APIが動作するよう修正

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
+    _ "github.com/go-sql-driver/mysql"
+    "index/controller"
+    "index/middleware"
 )
 
-func main() {
-	engine := gin.Default()
-	userAgent := ""
-
-	engine.Use(func(c * gin.Context) {
-		userAgent = c.GetHeader("User-Agent")
-		c.Next()
-	})
-
-	engine.GET("/", func(c * gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello world",
-			"User-Agent": userAgent,
-		})
-	})
-	engine.Run(":3000")
+func main(){
+    engine := gin.Default()
+    engine.Use(middleware.RecordUaAndTime)
+    bookEngine := engine.Group("/book")
+    {
+        v1 := bookEngine.Group("/v1")
+        {
+            v1.POST("/add", controller.BookAdd)
+            v1.GET("/list", controller.BookList)
+            v1.PUT("/update", controller.BookUpdate)
+            v1.DELETE("/delete", controller.BookDelete)
+        }
+    }
+    engine.Run(":3000")
 }
